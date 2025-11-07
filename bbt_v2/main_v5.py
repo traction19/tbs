f69394b6dc79676854a8419c47ce29a7c93413e5
breakout <kunzeleven@gmail.com>
2025-11-07 13:57:58 +0700

    Update main_v5.py

--- bbt_v2/main_v5.py
+++ bbt_v2/main_v5.py
@@ -309,7 +309,7 @@ def booking_weekly_page() -> None:
 
     # 2) Fallback: kalau DB kosong / gagal, pakai daftar default (sesuaikan jika perlu)
     if not ruang_options:
-        ruang_options = ["Breakout DigiAds", "Coordination", "Cozy 19.2", "Cozy 19.3", "Cozy 19.4"]
+        ruang_options = ["Breakout Traction","Breakout DigiAds","Dedication 1","Dedication 2","Dedication 3","Dedication 5","Dedication 6","Coordination","Cozy 19.2","Cozy 19.3","Cozy 19.4"]
 
     with st.form("weekly_booking_form", clear_on_submit=False):
         nama = st.text_input("Nama Pemesan")
